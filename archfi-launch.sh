2c2993c416074f453a2b0696545c63cefb092a34
johnkunai <64452464+johnkunai@users.noreply.github.com>
2020-05-21 21:28:22 -0600

    Update archfi-launch.sh

--- archfi-launch.sh
+++ archfi-launch.sh
@@ -1,6 +1,6 @@
 # In order to change the country, just change United States to your desired countries name. Hope this helps! :D
 #! bin/bash
-pacman -Syy reflector
+pacman -Syy --needed reflector
 reflector -c 'United States' --sort rate --save /etc/pacman.d/mirrorlist
 wget archfi.sf.net/archfi
 sh archfi
